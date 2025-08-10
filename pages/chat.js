a91e2da884db68a25b0893f1c6b02385c1fa278e
Nest-Ed <preet119.pk@gmail.com>
2025-08-10 22:47:28 +1200

    Create chat.js

---
+++ pages/chat.js
@@ -0,0 +1,10 @@
+// pages/chat.js
+import ChatBox from "../components/ChatBox";
+
+export default function ChatPage() {
+  return (
+    <main style={{ fontFamily: "system-ui, -apple-system, Segoe UI, Roboto" }}>
+      <ChatBox />
+    </main>
+  );
+}
