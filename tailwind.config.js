16f8dfe77cf28a6260088e7e4f9e284ace452cfc
Nest-Ed <preet119.pk@gmail.com>
2025-08-31 17:14:07 +1200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./styles/**/*.{css,scss}", // add styles folder so globals.css is included
   ],
   theme: {
     extend: {},
